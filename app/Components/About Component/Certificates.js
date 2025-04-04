741e83355a9dd6a95aea3c458d8ebfff40a24976
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2025-04-04 06:45:40 +0000

    Improved Performance

--- app/Components/About Component/Certificates.js
+++ app/Components/About Component/Certificates.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Certificates = () => {
   return (
     <>
--- app/Components/ProductComponent/ProductCard.js
+++ app/Components/ProductComponent/ProductCard.js
@@ -1,36 +1,27 @@
-// import React from "react";
 
-// const ProductCard = ({ type, name }) => {
-//   return (
-//     <div>
-//       <div>
-//         <img
-//           className="h-[420px] w-[20dvw] rounded-2xl border-4 border-blue-200 object-cover"
-//           src={`/${type}/${name}.webp`}
-//           alt={`${type}${name}`}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
 
-// export default ProductCard;
-
-"use client"; // Marks this file as a Client Component
+"use client";
 
 import React, { useState } from "react";
+import Image from 'next/image';
 
 const ProductCard = ({ type, name }) => {
   const [imgSrc, setImgSrc] = useState(`/${type}/${name}.webp`);
 
   return (
     <div className="w-[250px] flex flex-col items-center backgroundCard p-4 shadow-lg rounded-2xl border border-gray-200 transition-all duration-300 hover:scale-105 hover:shadow-xl">
-      <img
+      <Image
         className="h-[250px] w-full rounded-xl border border-blue-200 object-cover"
         src={imgSrc}
         alt={`${type} ${name}`}
-        onError={() => setImgSrc("/placeholder.webp")} // Fallback image
+        width={500} 
+        height={250} 
+        priority 
+        placeholder="blur" 
+        blurDataURL="/placeholder.webp" 
+        onError={() => setImgSrc("/placeholder.webp")}
       />
+      {/* Commented for Future Use */}
       {/* <h3 className="mt-2 text-lg font-semibold text-gray-700">
         {name.replace(/p/, "Product ")}
       </h3> */}
